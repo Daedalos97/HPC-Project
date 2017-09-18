3153aa51965b697b6acbfecbc8557651476e23cf
Samuel <sam.j.s.heath@gmail.com>
2017-09-18 14:48:46 +0800

    Added base file with args

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <omp.h>
+#include <getopt.h>
+
+#define SIZE 128
+#define OPTLIST "p:sb"
+
+bool pflag = false;
+bool sflag = false;
+bool bflag = false;
+
+double prob;
+
+typedef struct lattice {
+	bool sites[SIZE][SIZE];
+} LATTICE;
+
+LATTICE lattice;
+
+void seed_lattice() {
+	#pragma omp parallel for num_threads(8) collapse(2)
+		for (int i = 0; i < SIZE; i++) {
+			for (int j = 0; j < SIZE; j++) {
+				if ((double) (rand()/(RAND_MAX+1.0)) < prob) {
+					lattice.sites[i][j] = true;
+				}
+			}
+		}
+}
+
+void find_bonds() {
+
+
+}
+
+int main(int argc, char *argv[]) {
+	int opt;
+	while ((opt = getopt(argc,argv,OPTLIST)) != -1) {
+		switch (opt) {
+			case 'p': 
+				if (isdigit(optarg[0])) {
+					prob = strtod(optarg,NULL);
+					pflag = true;
+				} else {
+					fprintf(stderr, "Invalid probability argument.\n");
+					exit(EXIT_FAILURE);
+				}
+				break;
+			case 's':
+				sflag = true;
+				break;
+			case 'b':
+				bflag = true;
+				break;
+			case '?': fprintf(stderr, "Invalid command.\n"); exit(EXIT_FAILURE);
+		}
+	}
+	if (sflag == bflag) {
+		fprintf(stderr, "You need only one -s or -b flag for either site or bond percolation.\n");
+		exit(EXIT_FAILURE);
+	} else if (!pflag) {
+		fprintf(stderr, "No percolation probability given.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	seed_lattice();
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) {
+			printf("%d ", lattice.sites[i][j]);
+		}
+		printf("\n");
+	}
+
+	exit(EXIT_SUCCESS);
+}
