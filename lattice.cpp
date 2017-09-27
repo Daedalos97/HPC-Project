0df9117fa41163bb7813cbc34c6dc89583b265be
Perth155 <abrar.a.amin@gmail.com>
2017-09-27 15:23:34 +0800

    updated lattice

--- lattice.cpp
+++ lattice.cpp
@@ -1,6 +1,52 @@
 #include "lattice.h"
 
-void printVisual()
+LATTICE lat;
+int local_len = 8;
+
+/**
+ * Prints the existing lattice, takes the length of the 2D array 
+ * as argument. If 'v' is passed in as print type a visual representation is 
+ * provided, any other character defaults to 1s and 0s. 
+ */
+void print_lattice(int len, char viewType)
 {
-	
+	for(int i = 0; i < len; i++)
+	{
+		for(int j = 0; j < len; j++)
+		{
+			if(viewType == 'v' || viewType == 'V'){
+				if(lat.lattice_array[i][j] == 1)
+					printf("#");
+				else if(lat.lattice_array[i][j]==2)
+					printf(" ");
+				else
+					printf(" ");
+			}else
+				printf(lat.lattice_array[i][j]);
+		}
+	}
+}
+
+
+
+/**
+ * Initialize the lattice struct to specified length.
+ */
+void init_lattice(int len)
+{
+	local_len = len;
+	if(len <= 1){
+		fprintf(stderr, "%d is an invalid lattice size. Must be greater than 1", len);
+		return;
+	}
+	lat.lattice_dimension = len;
+	lat.lattice_array = (int**) malloc(len*sizeof(int*));
+	for(int i = 0; i < len; i++){
+		lattice_array[i] = (int*) malloc(len*sizeof(int));
+	}
+}
+
+void destroy_lattice()
+{
+
 }
